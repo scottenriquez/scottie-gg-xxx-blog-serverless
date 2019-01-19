0da3e5132bbe253c96b55c9e88b681224961bee7
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-01-19 15:22:45 -0600

    Reformatted response for Lambda proxy integration; added query string support for post IDs

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const blogService = require('./services/Blog');
 
 exports.handler = async (event) => {
-    return await blogService.getAllBlogPosts();
+    let result = {};
+    if (event['queryStringParameters'] && event['queryStringParameters']['postID']) {
+        result = await blogService.getBlogPostForID(event['queryStringParameters']['postID']);
+    }
+    else {
+        result = await blogService.getAllBlogPosts();
+    }
+    const response = {
+        'isBase64Encoded': false,
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': JSON.stringify(result)
+    };
+    return response;
 };
